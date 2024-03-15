100fecb29a3bff8dcae101bab822a8271495994c
MaestroOG <muneeburrehman822@gmail.com>
2024-03-15 15:10:53 +0500

    add social links

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,6 +8,24 @@ import { FaLinkedin } from "react-icons/fa6";
 import './Card.css'
 
 const Card = () => {
+    const onGithub = () => {
+        location.href = "https://github.com/MaestroOG"
+    }
+
+    const onInsta = () => {
+        location.href = "https://www.instagram.com/ig._flynn?igsh=YnFleTd2aDA2Znll"
+    }
+
+    const onWhatsApp = () => {
+        location.href = "https://wa.me/qr/HBXECR2NOB5RF1"
+    }
+
+    const onLinkedIn = () => {
+        location.href = "https://linkedin.com/in/muneeb-ur-rehman-6122b4292/"
+    }
+
+
+
     return (
         <>
             <div className="card-container">
@@ -15,15 +33,15 @@ const Card = () => {
                     <img src={Avatar} alt="avatar" />
                     <h1>Muneeb Ur Rehman</h1>
                     <h3>Faisalabad, Pakistan</h3>
-                    <div className="bio">"Front-End Developer and avid reader."</div>
+                    <div className="bio">"Full-Stack Developer and avid reader."</div>
                 </div>
                 <div className="links">
                     <ul>
-                        <li><FaGithub />{" "}Github</li>
-                        <li><FaInstagram />{" "}Instagram</li>
-                        <li><FaWhatsapp />{" "}Whatsapp</li>
+                        <li onClick={onGithub}><FaGithub />{" "}Github</li>
+                        <li onClick={onInsta}><FaInstagram />{" "}Instagram</li>
+                        <li onClick={onWhatsApp}><FaWhatsapp />{" "}Whatsapp</li>
                         <li><FaXTwitter />{" "}Twitter</li>
-                        <li><FaLinkedin />{" "}LinkedIn</li>
+                        <li onClick={onLinkedIn}><FaLinkedin />{" "}LinkedIn</li>
                     </ul>
                 </div>
             </div>
